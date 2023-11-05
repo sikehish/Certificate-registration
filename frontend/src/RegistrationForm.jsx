4b20e36bb67c86675ee7fecb393d135810dd3d8b
Aryan-Kumar-Thakur <thakuraryan7678@gmail.com>
2023-11-05 23:19:09 +0530

    Authentication and data store in firebase

--- frontend/src/RegistrationForm.jsx
+++ frontend/src/RegistrationForm.jsx
@@ -3,6 +3,27 @@ import { TextField, Button, Card, CardContent, Grid, Typography, Container, Box,
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAT40Ds2iJYrwMpB9LvZAhrFu0pylUIPQ",
+  authDomain: "certificate-registration.firebaseapp.com",
+  databaseURL: "https://certificate-registration-default-rtdb.firebaseio.com",
+  projectId: "certificate-registration",
+  storageBucket: "certificate-registration.appspot.com",
+  messagingSenderId: "417408201765",
+  appId: "1:417408201765:web:2d8150e290580e7e137bcc"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
 const RegistrationForm = () => {
   const [formData, setFormData] = useState({
     email: '',
@@ -16,18 +37,51 @@ const RegistrationForm = () => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleRegistration = () => {
+
+  const storeUserDataInFirestore = async (userId, formData) => {
+    // Store user-related data in Firestore
+    const userDocRef = doc(db, "users", userId);
+    await setDoc(userDocRef, {
+      email: formData.email,
+      usn: formData.usn,
+      password: formData.password,
+      confirmPassword: formData.confirmPassword
+      // Add other fields as needed
+    });
+  };
+
+  const handleRegistration = async (e) => {
     if (formData.password !== formData.confirmPassword) {
       toast.error("Passwords do not match");
     } else {
-      // Implement your validation and database logic here
-      // Display success message if registration is successful
-      toast.success("Successfully registered");
+      try {
+        // Use Firebase Authentication to register the user
+        const userCredential = await createUserWithEmailAndPassword(auth, formData.email, formData.password);
+
+        // User has been successfully registered
+        const user = userCredential.user;
+        toast.success("Successfully registered");
+
+         //reset input field
+
+        setFormData({
+          email: '',
+          usn: '',
+          password: '',
+          confirmPassword: '',
+        })
+
+        // Store user-related data in Firestore if needed
+        await storeUserDataInFirestore(user.uid, formData);
+      } catch (error) {
+        console.error('Error registering the user:', error);
+        toast.error("Registration failed , Email already exist");
+      }
     }
   };
 
   return (
-    <Container maxWidth="sm" style={{ display: 'flex',flexDirection:"column",  alignItems: 'center', justifyContent: 'center', textAlign: 'center', margin:"auto" }}>
+    <Container maxWidth="sm" style={{ display: 'flex', flexDirection: "column", alignItems: 'center', justifyContent: 'center', textAlign: 'center', margin: "auto" }}>
       <Box mt={4} display="flex" alignItems="center" justifyContent="center">
         <Avatar src="/gdsc-logo.png" alt="GDSC Logo" sx={{ width: 100, height: 100 }} />
         <Box ml={2}>
@@ -46,6 +100,7 @@ const RegistrationForm = () => {
                   fullWidth
                   value={formData.email}
                   onChange={handleInputChange}
+                  required
                 />
               </Grid>
               <Grid item xs={12}>
@@ -55,6 +110,7 @@ const RegistrationForm = () => {
                   fullWidth
                   value={formData.usn}
                   onChange={handleInputChange}
+                  required
                 />
               </Grid>
               <Grid item xs={12}>
@@ -65,6 +121,7 @@ const RegistrationForm = () => {
                   fullWidth
                   value={formData.password}
                   onChange={handleInputChange}
+                  required
                 />
               </Grid>
               <Grid item xs={12}>
@@ -75,6 +132,7 @@ const RegistrationForm = () => {
                   fullWidth
                   value={formData.confirmPassword}
                   onChange={handleInputChange}
+                  required
                 />
               </Grid>
               <Grid item xs={12}>
